df8ce56507982a1ccab04fb15b3d11c0631d87ae
artorias <agustincastro,irb@gmail.com>
2022-06-30 22:10:34 -0300

    update: clase y sprate

--- models/enemies.js
+++ models/enemies.js
@@ -1,23 +1,18 @@
-import { stick, claws } from "./weapons.js"
+import { stick, claws } from "./weapons.js";
+import { Personajes } from "./Personaje.js";
+import { poke133 } from "../asciiPokemons/eevee.js";
+import { poke143 } from "../asciiPokemons/snorlax.js";
 
-const minotaur = {
-    hp: 900,
-    strength: 10,
-    defense: 10,
-    nombre: 'minotaur', 
-    level: 1,
-    experience: 100,
-    weapon: stick
+class Enemigo extends Personajes {
+  constructor(vital, stren, def, name, lv, exp, arm, imagen) {
+    super(vital, stren, def, name, lv, exp, arm, imagen);
+  }
+  persar() {
+    let ase = 25;
+  }
 }
+const Eevee = new Enemigo(40, 40, 40, "Eevee", 1, 0, claws, poke133[0]);
 
-const harpy = {
-    hp: 1100,
-    strength: 20,
-    defense: 10,
-    nombre: 'harpy',
-    level: 2,
-    experience: 200,
-    weapon: claws
-}
+const Snorlax = new Enemigo(50, 50, 50, "Snorlax", 1, 0, stick, poke143[0]);
 
-export {minotaur, harpy};
+export { Eevee, Snorlax };
